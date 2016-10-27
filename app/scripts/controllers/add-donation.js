4bb7a1d6e5e5315cab1d55b822cca7d4be47469d
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-10-27 12:43:51 +0530

    Addded donor validation for cash donations

--- app/scripts/controllers/add-donation.js
+++ app/scripts/controllers/add-donation.js
@@ -60,9 +60,11 @@ angular.module('donutApp')
 			if(User.checkLoggedIn()) {
 				var fundraiser_id = User.getUserId();
 
+
+				//Validate donation
 				$http({
 					method: 'POST',
-					url: $rootScope.base_url + 'donation/add',
+					url: $rootScope.base_url + 'donation/validate',
 					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 					transformRequest: function(obj) {
 						var str = [];
@@ -70,15 +72,26 @@ angular.module('donutApp')
 						return str.join('&');
 					},
 					data: {amount : vm.donation.amount, donor_name : vm.donation.name, donor_email : vm.donation.email,
-						donor_phone : vm.donation.phone, eighty_g_required : vm.donation.eighty_g, address : vm.donation.address, 
-						comment: vm.donation.comment, fundraiser_id : fundraiser_id, 
+						donor_phone : vm.donation.phone, eighty_g_required : vm.donation.eighty_g, address : vm.donation.address,
+						comment: vm.donation.comment, fundraiser_id : fundraiser_id,
 						created_at : $filter("date")(vm.donation.created_at, "yyyy-MM-dd"), format : 'json'}
 
 				}).success(function (data) {
-					vm.is_processing = false;
 
-					var alert = $mdDialog.alert().title('Success!').content('Donation successfully added. ID: ' + data.donation.id).ok('Ok');
-					$mdDialog.show(alert);
+					console.log(data);
+					if(data.success){
+
+						//insertDonation();
+
+					}else{
+						var confirm_dialog = $mdDialog.confirm().title('Confirm').content(data.error).ok('Yes').cancel('No');
+						$mdDialog.show(confirm_dialog).then(function() {
+							//insertDonation();
+						}, function() {
+							vm.is_processing = false;
+						});
+					}
+
 
 				}).error(function (data) {
 					vm.is_processing = false;
@@ -87,6 +100,38 @@ angular.module('donutApp')
 					var alert = $mdDialog.alert().title('Error!').content('Connection issue with \''+$rootScope.base_url + 'donation/add'+'\'. Please try again later.').ok('Ok');
 					$mdDialog.show(alert);
 				});
+
+
+
+				var insertDonation = function(){
+					$http({
+						method: 'POST',
+						url: $rootScope.base_url + 'donation/add',
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+						transformRequest: function(obj) {
+							var str = [];
+							for(var p in obj) { str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p])); }
+							return str.join('&');
+						},
+						data: {amount : vm.donation.amount, donor_name : vm.donation.name, donor_email : vm.donation.email,
+							donor_phone : vm.donation.phone, eighty_g_required : vm.donation.eighty_g, address : vm.donation.address,
+							comment: vm.donation.comment, fundraiser_id : fundraiser_id,
+							created_at : $filter("date")(vm.donation.created_at, "yyyy-MM-dd"), format : 'json'}
+
+					}).success(function (data) {
+						vm.is_processing = false;
+						console.log(data);
+						var alert = $mdDialog.alert().title('Success!').content('Donation successfully added. ID: ' + data.donation.id).ok('Ok');
+						$mdDialog.show(alert);
+
+					}).error(function (data) {
+						vm.is_processing = false;
+						vm.is_error = true;
+
+						var alert = $mdDialog.alert().title('Error!').content('Connection issue with \''+$rootScope.base_url + 'donation/add'+'\'. Please try again later.').ok('Ok');
+						$mdDialog.show(alert);
+					})
+				};
 
 			} else {
 				vm.is_processing = false;
@@ -97,7 +142,7 @@ angular.module('donutApp')
 
 			};
 
-			//Initializing fields to be empty otherwise fields contain undefined.
+/*			//Initializing fields to be empty otherwise fields contain undefined.
 			vm.donation = {};
 			vm.donation.name = "";
 			vm.donation.amount = "";
@@ -109,7 +154,7 @@ angular.module('donutApp')
 
 			//So that form is reset after submit
 			vm.donationForm.$setUntouched();
-			vm.donationForm.$setPristine();
+			vm.donationForm.$setPristine();*/
 		};
 
   }]);
