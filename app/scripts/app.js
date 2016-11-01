5c427f40ccb3e49db6c36737c18fed6945c3a974
Binny V A <binnyva@gmail.com>
2016-11-01 17:41:33 +0530

    Adding delete donation option(IN progress). Added reject donation option and more

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,6 +71,11 @@ angular
 		restricted : true,
 		title: "Approve Donations"
 		})
+	  .when('/approved-donations', {
+		templateUrl: 'views/approved-donations.html',
+		restricted : true,
+		title: "Approved Donations"
+		})
 	  .when('/change-password', {
 		templateUrl: 'views/change-password.html',
 		controller: 'ChangePasswordCtrl',
@@ -92,4 +97,30 @@ angular
 			else 
 				$rootScope.title = "Donut";
 		});
-	}]);
+	}])
+	.filter('dateToISO', function() {
+		return function(input) {
+			var t = input.split(/[- :]/); // Split timestamp into [ Y, M, D, h, m, s ]
+			var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]); // Apply each element to the Date function
+			return d.toISOString();
+		};
+	})
+	.filter('statusFormat', function() {
+		// :TODO:
+		// Make a mapping of all the possible status and a text for it - rather than just a formatting.
+		// 
+		return function(input) {
+			input = input.replace(/[_\-]/g, ' ')		//Changes 'hello_cruel-world' to 'hello cruel world'
+				.replace(/([a-zA-Z])(\d)/g, '$1 $2')	//Changes 'no1' to 'no 1'
+				.replace(/([a-z])([A-Z])/g, '$1 $2');	//Changes 'helloWorld' to 'hello World'
+
+			var text = input.toLowerCase().split(' ').map(function(word) {
+					return word.replace(word[0], word[0].toUpperCase());
+				}).join(' ');
+
+			text = text.replace('Poc', 'POC').replace('Fc', 'Finance Fellow');
+
+			return text;
+		};
+	});;
+
--- app/scripts/controllers/approved-donations.js
+++ app/scripts/controllers/approved-donations.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name donutApp.controller:ApprovalsCtrl
+ * @name donutApp.controller:ApprovedDonationsCtrl
  * @description
- * # ApprovalsCtrl
+ * # ApprovedDonationsCtrl
  * Controller of the donutApp
  */
 angular.module('donutApp')
-  .controller('ApprovalsCtrl', ['$http','$scope','$rootScope','$mdDialog','UserService','$location', '$cookies',
+  .controller('ApprovedDonationsCtrl', ['$http','$scope','$rootScope','$mdDialog','UserService','$location', '$cookies',
   			function($http,$scope,$rootScope,$mdDialog,User, $location, $cookies){
 		var vm = this;
 		vm.is_processing = true;
@@ -23,7 +23,11 @@ angular.module('donutApp')
 				method: 'GET',
 				url: $rootScope.base_url + "donation/get_donations_for_approval/" + poc_id,
 				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-				transformRequest: vm.transformRequest,
+				transformRequest: function(obj) {
+					var str = [];
+					for(var p in obj) { str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p])); }
+					return str.join('&');
+				}
 			}).success(function (data) {
 				vm.is_processing = false;
 
@@ -47,6 +51,7 @@ angular.module('donutApp')
 			var alert = $mdDialog.alert().title('Error!').content('Connection error. Please login again.').ok('Ok');
 			$mdDialog.show(alert);
 			$location.path('/login');
+
 		};
 
 		vm.transformRequest = function(obj) {
@@ -65,69 +70,38 @@ angular.module('donutApp')
 			return true;
 		}
 
-
-		vm.approveDonation = function(donation_id) {
-			vm.is_processing = true;
+		vm.rejectDonation = function(donation_id) {
+			if(!vm.userCheck()) return false;
 			vm.active_donation_id = donation_id;
-
-			if(!User.checkLoggedIn()) {
-				vm.is_processing = false;
-
-				var alert = $mdDialog.alert().title('Error!').content('Connection error. Please try again later.').ok('Ok');
-				$mdDialog.show(alert);
-				$location.path('/login');
-				return;
-			}
-
-			var poc_id = User.getUserId();
-
-			// Show this only once per day
-			if($cookies.get('daily_confirmation')) {
-				vm.approveDonationCall(); 
-				return;
-			}
 			
-			var confirm = $mdDialog.confirm().title('Collected?')
-				.content('Please ensure that you have collected Rs. ' + vm.donations[donation_id].amount + ' from ' + vm.donations[donation_id].user_name + '. Press \'Yes\' if already collected. Press \'No\' if not collected yet.')
+			var confirm = $mdDialog.confirm().title('Reject Donation?')
+				.content('Are you sure you want to reject the donation of Rs. ' + vm.donations[donation_id].amount + ' from ' + vm.donations[donation_id].user_name)
 				.ok('Yes').cancel('No');
-			$mdDialog.show(confirm).then(function() { // Yes
-				// No cookie - create cookie with one day expiry.
-				var expire_date = new Date();
-	  			expire_date.setDate(expire_date.getDate() + 1);
-				$cookies.put('daily_confirmation', '1', {'expires': expire_date});
-
-				vm.approveDonationCall();
-			}, function() { // No
-				vm.is_processing = false;
+			$mdDialog.show(confirm).then(vm.rejectDonationCall, function() { // No
+				
 			});
-
 		}
 
-		vm.approveDonationCall = function() {
+		vm.rejectDonationCall = function() {
+			vm.is_processing = true;
 			var donation_id = vm.active_donation_id;
+			var poc_id = User.getUserId();
+
 			$http({
 				method: 'GET',
-				url: $rootScope.base_url + "donation/" + donation_id + '/approve/' + poc_id,
+				url: $rootScope.base_url + "donation/" + donation_id + '/reject/' + poc_id,
 				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 				transformRequest: vm.transformRequest
 			}).success(function (data) {
 				vm.is_processing = false;
-				vm.donations[data.donation_id].donation_status = 'APPROVED';
+				vm.donations[data.donation_id].donation_status = 'TO_BE_APPROVED_BY_POC';
 				vm.active_donation_id = 0;
 
-				var alert = $mdDialog.alert().title('Success!').content('Donation Approved. ID: ' + data.donation_id).ok('Ok');
+				var alert = $mdDialog.alert().title('Success!').content('Donation Recjected. ID: ' + data.donation_id).ok('Ok');
 				$mdDialog.show(alert);
 
-			}).error(function (data) {
-				vm.is_processing = false;
-				vm.active_donation_id = 0;
-
-				var alert = $mdDialog.alert().title('Error!').content('Connection error. Please try again later.').ok('Ok');
-				$mdDialog.show(alert);
-
-			});
+			}).error(vm.connectionError);
 		}
-
 
 		// Delete donation option.
 		vm.deleteDonation = function(donation_id) {
@@ -160,4 +134,14 @@ angular.module('donutApp')
 
 			}).error(vm.connectionError);
 		}
+
+
+		vm.connectionError = function (data) {
+			vm.is_processing = false;
+			vm.active_donation_id = 0;
+
+			var alert = $mdDialog.alert().title('Error!').content('Connection error. Please try again later.').ok('Ok');
+			$mdDialog.show(alert);
+		}
   }]);
+
